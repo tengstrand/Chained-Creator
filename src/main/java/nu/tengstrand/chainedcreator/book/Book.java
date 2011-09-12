58781f9a03a837374e53350e3b132f8e88a36211
unknown <joakim.tengstrand@.jaybis.local>
2011-09-12 18:14:21 +0200

    Replaced with regular constructors for example 1 and 2.

--- src/main/java/nu/tengstrand/chainedcreator/book/Book.java
+++ src/main/java/nu/tengstrand/chainedcreator/book/Book.java
@@ -17,22 +17,18 @@ public class Book {
     Book() {
     }
 
-    Book(BookTitle title, BookBinding binding, BookAuthor author, BookNumberOfPages numberOfPages, BookWeightInGrams grams) {
+    // Example of a regular constructor, used in Example 1.
+    public Book(String title, BookBinding binding, String author, int numberOfPages, int grams) {
+        this(new BookTitle(title), binding, new BookAuthor(author), new BookNumberOfPages(numberOfPages), new BookWeightInGrams(grams));
+    }
+
+    // Example of a regular constructor, used in Example 2.
+    public Book(BookTitle title, BookBinding binding, BookAuthor author, BookNumberOfPages numberOfPages, BookWeightInGrams grams) {
         this.title = title;
         this.binding = binding;
         this.author = author;
         this.numberOfPages = numberOfPages;
         this.grams = grams;
-    }
-
-    // An ordinary constructor, sending in a list of primitive types.
-    public static Book create(String title, BookBinding binding, String author, int numberOfPages, int grams) {
-        return new Book(new BookTitle(title), binding, new BookAuthor(author), new BookNumberOfPages(numberOfPages), new BookWeightInGrams(grams));
-    }
-
-    // An ordinary constructor, sending in a list of value objects.
-    public static Book create(BookTitle title, BookBinding binding, BookAuthor author, BookNumberOfPages numberOfPages, BookWeightInGrams grams) {
-        return new Book(title, binding, author, numberOfPages, grams);
     }
 
     // Create a book by using the pattern Chained Creator.
